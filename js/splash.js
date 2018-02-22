87bc7b2cb9eb17870f7d001f46984911efcf4bd1
MelynaPernia <mebipeos@outlook.com>
2018-02-22 06:12:46 -0500

    mejorando el splash

--- js/splash.js
+++ js/splash.js
@@ -1,6 +1,19 @@
 /* Vista splash de index a sesion en 3 segundos */
-$(document).ready(function() {
-  setTimeout(function() { 
+$(document).ready(function () {
+  // setTimeout(function() { 
+  //   window.location.href = 'views/sesion.html';
+  // }, 3000);
+  $('.title').animate({
+    marginBottom: '5in',
+    color: '#f0dc5b'
+  }, 5000);
+  // animación del logo
+  $('.logo').animate({
+    opacity: 0,
+    marginBottom: '5in'
+  }, 5000);
+  // mostrar la vista main después de cinco segundos
+  setTimeout(function () {
     window.location.href = 'views/sesion.html';
-  }, 3000);
+  }, 5000);
 });
